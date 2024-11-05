9436c6a373259c1b2730cd1c2477b6b8e8e0dbcc
Meet Shah <meet30012002@gmail.com>
2024-11-05 13:44:58 -0500

    App.js setup

---
+++ App.js
@@ -0,0 +1,30 @@
+// App.js
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './screens/Home';
+import Borrowed from './screens/Borrowed';
+
+const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+
+function BooksStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="BooksList" component={BooksList} />
+      <Stack.Screen name="BookDetail" component={BookDetail} />
+    </Stack.Navigator>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={BooksStack} />
+        <Tab.Screen name="Borrowed" component={Borrowed} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
