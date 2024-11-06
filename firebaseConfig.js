c96d130135bbbb826ee3d2836dd13225871c7236
Meet Shah <meet30012002@gmail.com>
2024-11-06 17:27:41 -0500

    FolderStructure Changed

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,21 +1,19 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// firebaseConfig.js
+import { initializeApp } from 'firebase/app';
+import { getDatabase } from 'firebase/database';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCPierdy6hgaTynQA2cKJ6gxOBNxZDyrnA",
   authDomain: "sem3advtopicslab3.firebaseapp.com",
+  databaseURL: "https://sem3advtopicslab3-default-rtdb.firebaseio.com",
   projectId: "sem3advtopicslab3",
-  storageBucket: "sem3advtopicslab3.firebasestorage.app",
+  storageBucket: "sem3advtopicslab3.appspot.com",
   messagingSenderId: "931569171718",
   appId: "1:931569171718:web:3dc81777ed12e4ffca7287",
   measurementId: "G-1LPM6HLRHK"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const database = getDatabase(app);
+
+export { database };
